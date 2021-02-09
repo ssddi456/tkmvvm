3ebf306616e2ad96a96c83d142ff1e182130b2e3
ssddi456 <torrent88410@gmail.com>
2021-02-09 15:33:57 +0800

    Update viewmodel.py

--- tkmvvm/viewmodel.py
+++ tkmvvm/viewmodel.py
@@ -1,15 +1,17 @@
+# -*- coding: utf-8 -*-
+
 import abc
-import mvvm
-import model
+from .mvvm import NotifyPropertyChanged
+from .model import Model
 from typing import Union
 
 
-class ViewModel(mvvm.NotifyPropertyChanged, abc.ABC):
+class ViewModel(NotifyPropertyChanged, abc.ABC):
     # used to store current selection in a listbox widget
     current_selection = ()
 
     def on_property_changed(self, property_name: str):
         self._property_changed(property_name)
 
-    def __init__(self, model_: Union[None, model.Model]):
+    def __init__(self, model_: Union[None, Model]):
         self.model = model_
